ba4d7af02b7ea1d61ada5d796da888a7eda6056f
HarshGeed <harshgeed07@gmail.com>
2025-08-24 10:32:29 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { decrement, increment, incrementByAmount } from './features/counter/counterSlice';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0);
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  }
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  }
+
+  const handleIncrementBy = () => {
+    dispatch(incrementByAmount(amount));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleIncrement}>+</button>
+      <p>Count: {count}</p>
+      <button onClick={handleDecrement}>-</button>
+      <br /><br />
+      <input type="number" placeholder='enter the number' onChange={(e) => setAmount(e.target.value)} />
+      <br /><br />
+      <button onClick={handleIncrementBy}>Increment by </button>
     </>
   )
 }
